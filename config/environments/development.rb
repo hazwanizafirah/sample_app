4501e6b2556b1ebbb676d5a63d91f9d034050399
hazwanizafirah <hazwanizafirah78@gmail.com>
2021-09-03 18:51:48 +0800

    Chapter 11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,21 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: "localhost:3000" }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["USER_EMAIL"],
+    password: ENV["USER_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -35,7 +50,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  host = 'example.com' # Don't use this literally; use your local dev host instead
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_084121) do
+ActiveRecord::Schema.define(version: 2021_09_03_090026) do
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
