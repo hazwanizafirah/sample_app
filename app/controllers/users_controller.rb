9e1f1bc8636fc6fc65c7a48a8f9fd6406fcd0f56
hazwanizafirah <hazwanizafirah78@gmail.com>
2021-09-08 23:02:26 +0800

    Chapter 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :load_user, only: :show
-  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :logged_in_user, only: %i(index edit update destroy following, followers)
   before_action :correct_user, only: %i(edit update)
   before_action :admin_user, only: %i(destroy)
 
@@ -56,6 +56,20 @@ class UsersController < ApplicationController
       flash[:danger] = t(:delete_fail)
     end
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find_by(id: params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find_by(id: params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
   end
 
   private
